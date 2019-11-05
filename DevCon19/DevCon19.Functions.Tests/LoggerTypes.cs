8ce37da9080f0f3295a52a0c7e93670bf23a956e
Miguel Arcilla <miguel.arcilla@live.com>
2019-11-05 10:55:35 +0800

    Added base Unit Test project and test functions

---
+++ DevCon19/DevCon19.Functions.Tests/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevCon19.Functions.Tests
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
---
+++ DevCon19/DevCon19.Functions.Tests/NullScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevCon19.Functions.Tests
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        private NullScope() { }
+        public void Dispose() { }
+    }
+}
