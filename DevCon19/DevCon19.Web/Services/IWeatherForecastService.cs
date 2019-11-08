545d7884752836044d1aa1ba4c7e1ea2ff6d066f
Miguel Arcilla <miguel.arcilla@live.com>
2019-11-08 16:02:54 +0800

    Updated DevCon Web project to use Weather app, no functions integration

---
+++ DevCon19/DevCon19.Web/Services/IWeatherForecastService.cs
@@ -0,0 +1,18 @@
+﻿using DevCon19.Web.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevCon19.Web.Services
+{
+    public interface IWeatherForecastService
+    {
+        Task<Location> GetLocationByGeolocation(decimal latitude, decimal longitude);
+
+        Task<Location[]> GetLocationsByText(string search, CancellationToken cancellationToken);
+
+        IAsyncEnumerable<WeatherResponse> GetStreamingWeather(string locationKey, CancellationToken token);
+
+        Task<WeatherResponse> GetWeather(string locationKey);
+    }
+}
--- DevCon19/DevCon19.Web/Startup.cs
+++ DevCon19/DevCon19.Web/Startup.cs
@@ -1,5 +1,9 @@
+using Blazored.LocalStorage;
+using DevCon19.Web.Services;
 using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using GeolocationService = AspNetMonsters.Blazor.Geolocation.LocationService;
 
 namespace DevCon19.Web
 {
@@ -7,6 +11,11 @@ namespace DevCon19.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IWeatherForecastService, HttpWeatherForecastService>();
+            services.AddScoped<GeolocationService>();
+            services.AddBlazoredLocalStorage();
+            services.AddScoped<PinnedLocationsService>();
+            services.AddScoped<IConfiguration, LocalStorageConfiguration>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
